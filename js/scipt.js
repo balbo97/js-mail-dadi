92eba88785939f9533cde6aabc8817c9b973d577
balbo97 <balbosauro1@gmail.com>
2023-11-23 21:29:44 +0100

    DADI completed

--- js/scipt.js
+++ js/scipt.js
@@ -1,7 +1,7 @@
-const button = document.querySelector('.btn');
+const button_send = document.querySelector('.send');
 
 
-button.addEventListener('click', function(){
+button_send.addEventListener('click', function(){
     // RICHIEDERE L'EMAIL
     let email = document.getElementById('email').value; 
 
@@ -11,12 +11,40 @@ button.addEventListener('click', function(){
     // VERIFICARE SE LA MAIL INSERITA DALL'UTENTE E' PRESENTE NELL'ARRAY
     let right_email = false;
     for (let i=0; i<array.length; i++){
-
         if (email == array[i]){
-
+            
+            // E' PRESENTE 
             right_email = true 
+            
+            // LA INSERISCO NEL HTML
+            document.getElementById('welcome').innerHTML = `<span class="text-success"> Complimenti la tua email è corretta! </span>`
         }
     }
 
+    //STAMPO IN CONSOLE SE APPARTIENE O MENO ALL' ARRAY
     console.log(right_email)
+    
+   
+})
+
+
+const button_play = document.querySelector('.send');
+
+button_play.addEventListener('click', function(){
+    
+    const player = Math.ceil(Math.random()*6)
+    const computer = Math.ceil(Math.random()*6)
+
+    let win;
+    if (player>computer){
+        win = "player win"
+    }
+    else if(player<computer){
+        win = 'computer win'
+    }
+    else{
+        win = 'pareggio '
+    }
+   
+    document.getElementById('winner').innerText = (win) 
 })
